1da00df9f5c218a6268c5cd9d78ff93840a35a92
Muhammad Abdullah <abdullah.webdev9176@gmail.com>
2026-07-01 12:35:54 +0500

    updated form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,13 +72,19 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 // ...
  
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+ const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   await sql`
